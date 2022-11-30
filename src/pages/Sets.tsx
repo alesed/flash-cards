01dc06e712b5edcde635dc8038e308cefcdcd0e3
patrikma <patrikma@seznam.cz>
2022-11-30 23:24:32 +0100

    Sets page created

--- src/pages/Sets.tsx
+++ src/pages/Sets.tsx
@@ -1,5 +1,41 @@
-import { FC } from 'react';
+import { getDocs, onSnapshot } from 'firebase/firestore';
+import { FC, useEffect, useState } from 'react';
 
-const Sets: FC = () => <>Sets working</>;
+import SetsList from '../components/SetsList';
+import {
+	flashcardsCollection,
+	FlashcardsSet,
+	setsCollection
+} from '../utils/firebase/db';
+
+export type FlashcardsSetWithStats = FlashcardsSet & {
+	id: string;
+	flashCardsCount: number;
+};
+
+const Sets: FC = () => {
+	const [sets, setSets] = useState<FlashcardsSetWithStats[] | null>(null);
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(setsCollection, async snapshot => {
+			const flashcards = await getDocs(flashcardsCollection);
+			setSets(
+				snapshot.docs
+					.map(doc => ({ ...doc.data(), id: doc.id }))
+					.map(data => ({
+						...data,
+						flashCardsCount: flashcards.docs.filter(
+							doc => doc.data().setId === data.id
+						).length
+					}))
+			);
+		});
+		return () => {
+			unsubscribe();
+		};
+	});
+
+	return <SetsList sets={sets} title="All sets" />;
+};
 
 export default Sets;
