66925a78b5f6dbabec811e3826f37be7792c7e02
patrikma <patrikma@seznam.cz>
2022-11-30 23:24:43 +0100

    UserSets page created

---
+++ src/pages/UserSets.tsx
@@ -0,0 +1,44 @@
+import { getDocs, onSnapshot } from 'firebase/firestore';
+import { FC, useEffect, useState } from 'react';
+
+import SetsList from '../components/SetsList';
+import useLoggedInUser from '../hooks/useLoggedInUser';
+import {
+	flashcardsCollection,
+	FlashcardsSet,
+	setsCollection
+} from '../utils/firebase/db';
+
+export type FlashcardsSetWithStats = FlashcardsSet & {
+	id: string;
+	flashCardsCount: number;
+};
+
+const UserSets: FC = () => {
+	const [sets, setSets] = useState<FlashcardsSetWithStats[] | null>(null);
+	const user = useLoggedInUser();
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(setsCollection, async snapshot => {
+			const flashcards = await getDocs(flashcardsCollection);
+			setSets(
+				snapshot.docs
+					.map(doc => ({ ...doc.data(), id: doc.id }))
+					.filter(set => set.userId === user?.uid)
+					.map(data => ({
+						...data,
+						flashCardsCount: flashcards.docs.filter(
+							doc => doc.data().setId === data.id
+						).length
+					}))
+			);
+		});
+		return () => {
+			unsubscribe();
+		};
+	});
+
+	return <SetsList sets={sets} title="All sets" />;
+};
+
+export default UserSets;
